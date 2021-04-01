47630178c16f27c2d1b93f3f557800ee802ef052
21harley <jhonharleyllanesescobar@hotmail.com>
2021-04-01 03:06:30 -0600

    .

--- script.js
+++ script.js
@@ -1,18 +1,71 @@
-
+const mensaje=[
+    "we are an expert group in architectural design and interior design with a residential approach,where we generate creative,authentic and innovative soluction.achieving results that exceed the expectations of each client",
+    "the world is incredible, especially nature, we seek to create structures that are simple,functional and save materials in order to have a more sustainable future.",
+    "Price has no cost when the final product can exceed the consumer's expectations. A brand is transcended by the work and time invested in making a quality product added to the value of the legs."  
+];
+const ruta=[
+    "person_5.jpg",
+    "person_3.jpg",
+    "person_4.jpg",
+    "person_2.jpg",
+    "person_1.jpg",
+];
+const personas=[
+    "Haley Lennon",
+    "Georgia Gates",
+    "Jeff Freshman",
+    "Marian Donldson",
+    "John LLanes"
+];
 window.addEventListener("click",(e)=>{
+    console.log(e.target);
    if(e.target.matches(".main-services-ab-button")){
        const active=document.querySelector(".button-active");
+       const text=document.querySelector(".main-services-ab-p");
        active.classList.remove("button-active");
        e.target.classList.add("button-active");
+        switch(e.target.innerHTML){
+            case "Our Mission":
+                text.innerHTML=mensaje[0];
+            break;
+            case "Our Vision":
+                text.innerHTML=mensaje[1];
+            break;
+            case "Our Value":
+                text.innerHTML=mensaje[2];
+            break;
+        }
    }
+   if(e.target.matches(".radio-img")){
+       let ru;
+       let na;
+       switch(e.target.value){
+           case "primero":
+                ru=ruta[0];na=personas[0];
+            break;
+           case "segundo":
+                ru=ruta[1];na=personas[1];
+            break;
+           case "tercero":
+                ru=ruta[2];na=personas[2];
+            break;
+           case "cuarto":
+                ru=ruta[3];na=personas[3];
+            break;
+           case "quinto":
+                ru=ruta[4];na=personas[4];
+            break;
+       }
+       const per=document.querySelector(".img-container-p");
+       per.style.backgroundRepeat="no-repeat";
+       per.style.background=`url(Assets/img/${ru})`;
+       per.style.backgroundSize="cover";
+       const name=document.querySelector(".v-img-name");
+       name.innerHTML=na;
+   }
+
 });
 
-/*
-the world is incredible, especially nature, 
-we seek to create structures that are simple,
-functional and save materials in order to have
-a more sustainable future.
-*/
 window.onscroll = function (){
     let valor;
     if(window.screen.width>880){
@@ -21,7 +74,7 @@ window.onscroll = function (){
         valor=[150,700,1296,2515,3217,4594];
     }
     const scroll = document.documentElement.scrollTop || document.body.scrollTop;
-    console.log(scroll);
+    //console.log(scroll);
     if(scroll < valor[0] ){
         const home=document.querySelector(".header-nav-ul");
         home.classList.remove("move-m");
