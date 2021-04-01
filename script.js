b612145119c88eaf2560f86c1d1946944a404128
21harley <jhonharleyllanesescobar@hotmail.com>
2021-03-31 21:21:19 -0600

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("click",(e)=>{
    if(e.target.matches(".main-services-ab-button")){
        const active=document.querySelector(".button-active");
@@ -13,11 +14,19 @@ functional and save materials in order to have
 a more sustainable future.
 */
 window.onscroll = function (){
+    let valor;
+    if(window.screen.width>880){
+      //valor=[150,200,790,1509,2194,3712];
+      valor=[150,700,1296,2515,3217,4594];
+    }else{
+      //valor=[150,200,896,1715,2717,3794];
+    }
     const scroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if(scroll < 150 ){
+    console.log(scroll);
+    if(scroll < valor[0] ){
         const home=document.querySelector(".header-nav-ul");
         home.classList.remove("move-m");
-    }else if(scroll>=150){
+    }else if(scroll>=valor[0]){
         const home=document.querySelector(".header-nav-ul");
         home.classList.add("move-m");
     }
@@ -25,15 +34,15 @@ window.onscroll = function (){
     const link=document.querySelector(".nav-li-a-active");
     let aux;
     let cam=0;
-    if(scroll<=200){
+    if(scroll<=valor[1]){
         aux=document.querySelector("#a1");cam=1;
-    }else if(scroll<=790){
+    }else if(scroll<=valor[2]){
         aux=document.querySelector("#a2");cam=1;
-    }else if(scroll<=1509){
+    }else if(scroll<=valor[3]){
         aux=document.querySelector("#a3");cam=1;
-    }else if(scroll<=2194){
+    }else if(scroll<=valor[4]){
         aux=document.querySelector("#a4");cam=1;
-    }else if(scroll<=3172){
+    }else if(scroll<=valor[5]){
         aux=document.querySelector("#a5");cam=1;
     }
     if(cam!=0){
